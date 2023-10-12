c250e686dcb2090701769edde08dd74572bd1036
snowmeow2 <50443110+snowmeow2@users.noreply.github.com>
2023-10-12 16:29:14 +0800

    init commit

---
+++ arxiv-blue.user.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name         Blue arXiv Theme
+// @namespace    https://github.com/snowmeow2
+// @version      1.0
+// @description  Customize the appearance of arXiv website
+// @author       snowmeow2
+// @match        https://arxiv.org/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // Use custom css
+    const style = document.createElement('style');
+    style.innerHTML = `
+    #header {
+        background: linear-gradient(90deg, rgb(255, 255, 255), rgb(20, 100, 220), rgb(20, 100, 220));
+    }
+    .mobile-header {
+        background: linear-gradient(90deg, rgb(255, 255, 255), rgb(20, 100, 220), rgb(20, 100, 220));
+    }
+    .identity {
+        background: linear-gradient(90deg, rgb(255, 255, 255), rgb(20, 100, 220), rgb(20, 100, 220));
+    }
+    .header-breadcrumbs {
+        color: black !important;
+    }
+    .header-breadcrumbs a {
+        color: black !important;
+    }
+    .mobile-header .column {
+        border-left: 0px !important;
+        border-right: 0px !important;
+    }
+    `;
+    document.head.appendChild(style);
+
+    // Replace the logo image
+    const logoImg = document.querySelector('img[alt="arxiv logo"]');
+    if (logoImg) {
+        logoImg.src = 'https://i.imgur.com/ZkreQPV.png';
+        // check the url: If it is the homepage, don't resize the logo
+        if (!(window.location.href === 'https://arxiv.org/')) {
+            logoImg.style.width = '125px';
+        }
+        logoImg.style.margin = '0px';
+    }
+    const logoImg2 = document.querySelector('img[alt="arXiv logo"]');
+    if (logoImg2) {
+        logoImg2.src = 'https://i.imgur.com/ZkreQPV.png';
+    }
+    const logoCornell = document.getElementsByClassName('logo-cornell')[0];
+    if (logoCornell) {
+        logoCornell.style.visibility = 'hidden';
+    }
+
+    // Replace all text instances of "arXiv" with "Blue arXiv"
+    document.body.innerHTML = document.body.innerHTML.replace(/arXiv/g, 'Blue arXiv');
+})();
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Blue arXiv Theme
+藍色的 [arXiv](https://arxiv.org/) 主題。
+
+真正的檔案玩家
+![preview](https://i.imgur.com/ZkreQPV.png)
+
+## 安裝
+1. 確保你有安裝 [Tampermonkey](https://www.tampermonkey.net/) 或 [Greasemonkey](https://www.greasespot.net/) 或其他支援 userscript 的瀏覽器擴充功能。
+2. 將 [arxiv-blue.user.js](#) 的內容複製到新的 userscript 中。
+3. 確認你是否有啟用 userscript。
+4. 完成！
+
+## Credits
+@arkw0
+https://twitter.com/arkw0/status/1710635911506272326
